f288a99fa2c295a317b419c34ad78d26810079fa
Lawrence <lawrencehome21@gmail.com>
2023-07-04 12:01:09 -0700

    added login alerts

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
-$success = 0;
-$user = 0;
+$login = 0;
+$invalid = 0;
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     include 'connect.php';
     $userName = $_POST['username'];
@@ -9,14 +9,15 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 
     $sql = "select * from registration where username = '$userName' and password='$password'";
+
     $result = mysqli_query($con,$sql);
     if($result){
         $num=mysqli_num_rows($result);
         if($num>0){
-            echo "Logged in successfully";
+            $login = 1;
         }
         else{
-            echo "Invalid details";
+            $invalid = 1;
         }
     }
 
@@ -33,6 +34,25 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 </head>
 
 <body>
+    <?php
+        if($login){
+            echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Success </strong> You have successfully logged in.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>';
+        }
+    ?>
+
+    <?php
+        if($invalid){
+            echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Error </strong> invalid credentials.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>';
+        }
+    ?>
+
+
     <h1 class="text-center">login to our website</h1>
     <div class="container mt-5">
         <form action="login.php" method="post">
